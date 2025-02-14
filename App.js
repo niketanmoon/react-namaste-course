e654909b7f6cee31950fff6b1f80ae720f7a0d05
niketanmoon <niketanmoon@gmail.com>
2025-02-15 07:07:40 +1030

    Done with Chapter 3

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  { id: "container" },
-  "Hello Everyone!"
+// const heading = React.createElement(
+//   "h1",
+//   { id: "container" },
+//   "Hello Everyone!"
+// );
+const heading2 = (
+  <h1 id="container" key="h1">
+    Namaste React
+  </h1>
 );
+
+const Title = () => {
+  return <h1 id="title">Hello Title</h1>;
+};
+const HeaderComponent = () => {
+  return (
+    <header>
+      {/* {Title()} */}
+      <Title />
+      <h1>Header</h1>
+    </header>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeaderComponent />);
